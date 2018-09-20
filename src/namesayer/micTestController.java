bb18c5fccd9b67b829f9cc24f21684316ddd8531
Jordan Sim-Smith <jordansimsmith@gmail.com>
2018-09-21 11:20:20 +1200

    Bound MicWorker task object to the mic test progress bar

--- src/namesayer/micTestController.java
+++ src/namesayer/micTestController.java
@@ -1,5 +1,24 @@
 package namesayer;
 
-public class micTestController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ProgressBar;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class micTestController implements Initializable {
+
+    @FXML
+    private ProgressBar volumeBar;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        MicWorker worker = new MicWorker();
+
+        volumeBar.progressProperty().bind(worker.progressProperty());
+
+        new Thread(worker).start();
+    }
 }
