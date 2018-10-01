5fb9323d10ad69d2d6de1ff887a73a0359298c4c
Jordan Sim-Smith <jordansimsmith@gmail.com>
2018-10-02 12:26:26 +1300

    Added NameList encapsulated collection

---
+++ src/namesayer/NameList.java
@@ -0,0 +1,32 @@
+package namesayer;
+
+import java.util.List;
+
+public class NameList {
+
+    private List<Name> names;
+
+    public NameList(List<Name> names) {
+        this.names = names;
+    }
+
+    public void add(Name name) {
+        names.add(name);
+    }
+
+    public Name get(int index) {
+        return names.get(index);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+
+        for (Name name: names ) {
+            builder.append(name);
+            builder.append(" ");
+        }
+
+        return builder.toString();
+    }
+}
