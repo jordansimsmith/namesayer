36addebaf96bb83550d8b22442ca1cc2b4e530d0
Joshua <joshua.sheng.fu@gmail.com>
2018-10-11 13:57:24 +1300

    Streak System

--- src/namesayer/Main.java
+++ src/namesayer/Main.java
@@ -4,8 +4,15 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 import namesayer.model.ModelImpl;
+
+import java.io.*;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class Main extends Application {
 
@@ -22,6 +29,67 @@ public class Main extends Application {
         final boolean resizable = primaryStage.isResizable();
         primaryStage.setResizable(!resizable);
         primaryStage.setResizable(resizable);
+
+        int streak = 0;
+
+        // Get current date
+        Date today = new Date();
+
+
+        // Check text file for streaks
+        try {
+            Scanner scanner = new Scanner(new File("streak.txt"));
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String string = line.substring(0,29);
+                streak = Integer.parseInt(line.substring(30));
+                DateFormat format = new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy", Locale.ENGLISH);
+                Date dateFromFile = null;
+                try {
+                    dateFromFile = format.parse(string);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+
+//                System.out.println(dateFromFile);
+//                System.out.println(streak);
+
+
+                if ((dateFromFile.getYear() == today.getYear() && (dateFromFile.getMonth() == today.getMonth())) && (today.getDay() == dateFromFile.getDay()+1)) {
+                    streak++;
+                } else if ((dateFromFile.getYear() == today.getYear() && (dateFromFile.getMonth() == today.getMonth())) && (today.getDay() == dateFromFile.getDay())) {
+                    // same day
+                } else {
+                    // Clear streak
+                    streak = 0;
+                }
+
+            }
+
+        } catch (FileNotFoundException e) {
+            // File does not exist
+        }
+
+        // Create or overwrite streak file
+        try {
+            PrintWriter writer = new PrintWriter("streak.txt", "UTF-8");
+            writer.println(today + " " + streak);
+            writer.close();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+
+
+        // Pop up for streaks
+        if (streak >= 3) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Congratulations");
+            alert.setHeaderText("Streak!");
+            alert.setContentText("You have been practising names for " + streak + " days in a row!");
+            alert.showAndWait();
+        }
+
     }
 
 
@@ -31,5 +99,8 @@ public class Main extends Application {
         ModelImpl.getInstance();
 
         launch(args);
+
+
+
     }
 }
