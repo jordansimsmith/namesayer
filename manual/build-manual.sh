f9fe7496ed445871fa534b19f372d41e8b459dfb
Jordan Sim-Smith <jordansimsmith@gmail.com>
2018-10-18 10:45:03 +1300

    Created build script and directory for latex user manual

---
+++ manual/build-manual.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# delete existing build
+rm -rf build/
+
+# make new build folder
+mkdir build/
+
+# compile latex file
+pdflatex -output-directory build/ user-manual.tex
+
+# open pdf
+xdg-open build/user-manual.pdf
